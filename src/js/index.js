97c0bd2fb91d6dae1d9a6c3c8d7a9effa42189e8
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-14 21:24:53 -0400

    finished styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
 // Javascript Entry Point
+
+import $ from 'jquery';
+
+import formInfo from './formInfo';
+
+import { inputTemplate, selectTemplate, textAreaTemplate } from './templates';
+
+var $form = $('form');
+
+// var htmlForm = formInfo.map(function(anInput){
+// 	anInput = inputTemplate(anInput);
+// 	return anInput;
+// });
+
+
+
+var htmlForm = formInfo.map(function(anInput){
+
+	if (anInput.type === "text" || anInput.type === "email" || anInput.type === "tel"){
+		anInput = inputTemplate(anInput);
+			return anInput;
+	}else if (anInput.type === "select"){
+		anInput = selectTemplate(anInput);
+			return anInput;
+	}else if(anInput.type = "textarea"){
+		anInput = textAreaTemplate(anInput);
+			return anInput;
+	}
+	
+});
+
+// $form.append(htmlForm.join(''));
+
+$form.prepend(htmlForm.join(''));
+
---
+++ src/js/templates.js
@@ -0,0 +1,46 @@
+export function inputTemplate (inputElement){
+	
+
+	return `<div>
+				<label for="${inputElement.id}"><i class="fa ${inputElement.icon}"></i>${inputElement.label}<input type="${inputElement.type}" id="${inputElement.id}"></label>
+			</div>`;
+}
+
+
+export function textAreaTemplate (inputElement){
+	
+
+	return `<div class="text-area">
+				<label for="${inputElement.id}"><i class="fa ${inputElement.icon}"></i>${inputElement.label}<textarea type="${inputElement.type}" id="${inputElement.id}"></textarea></label>
+			</div>`;
+}
+
+
+
+export function selectTemplate (inputElement){
+
+	var arrOfOptions = inputElement.options;
+
+	var optionsCreator = arrOfOptions.map(function(anOption){
+			return `<option label="${anOption.label}" value="${anOption.value}"></option>`;
+		});
+	
+		
+	optionsCreator = optionsCreator.join('');
+
+	return `<div>
+					<select type="${inputElement.type}" id="${inputElement.id}">
+		  				<option label="${inputElement.label}" selected="true"></option>
+		  				${optionsCreator}
+					</select>
+			</div>`
+};
+
+// return `<div>
+// 				<label for="${inputElement.id}">Select language...
+// 					<select type="${inputElement.type}" id="${inputElement.id}" placeholder="${inputElement.label}">
+// 		  				${optionsCreator}
+// 					</select>
+// 				</label>
+// 			</div>`
+// };
